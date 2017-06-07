02cb8b8dd9b1b5bc3ac85d28b0fdfcca26feab58
nikolay-is <niki@vip.bg>
2017-06-07 10:24:20 +0300

    Passport configuration

--- server/config/express.js
+++ server/config/express.js
@@ -1,11 +1,28 @@
 const handlebars = require('express-handlebars')
 const express = require('express')
+const cookieParser = require('cookie-parser')
+const bodyParser = require('body-parser')
+const session = require('express-session')
+const passport = require('passport')
+
 
 module.exports = (app) => {
   app.engine('handlebars', handlebars({
     defaultLayout: 'main'
   }))
   app.set('view engine', 'handlebars')
+
+  app.set('view engine', 'handlebars')
+
+  app.use(cookieParser())
+  app.use(bodyParser.urlencoded({ extended: true }))
+  app.use(session({
+    secret: 'neshto-taino!@#$%',
+    resave: false,
+    saveUninitialized: false
+  }))
+  app.use(passport.initialize())
+  app.use(passport.session())
 
   app.use(express.static('public'))
 
